61629a3f753ed5c5ea2026d207c15a2ac28e0930
Ahn <anhpnnd@gmail.com>
2021-07-18 10:45:03 +0200

    upgrade project

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: 'react-native',
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.jsx$': 'babel-jest',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
